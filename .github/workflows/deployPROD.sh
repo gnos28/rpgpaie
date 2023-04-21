c87793841307d966002960ba0fc6dab23d52634e
gnos28 <gnos28@gmail.com>
2023-04-21 15:31:41 +0200

    fix deploy script

--- .github/workflows/deployPROD.sh
+++ .github/workflows/deployPROD.sh
@@ -14,18 +14,9 @@ mv rpgpaie/ old_rpgpaie/
 git clone git@github.com:gnos28/rpgpaie.git
 cd rpgpaie/
 # git pull -f --rebase origin main
-mkdir bdd/sql-files
 
 # récupérer les .env uploadés précédemment avec scp et les déplacer ici
-mv ../dotenv/.env .
-mv ../dotenv/.env.frontend frontend/.env.local
 mv ../dotenv/.env.backend backend/.env
-mv ../dotenv/.env.backend_cron backend_cron/.env
-
-# move old database to new folder
-mv ../old_rpgpaie/mariadb/ .
-# fix for "no permission to read" error
-sudo chmod -R 777 mariadb/
 
 # build docker images
 docker compose -f docker-compose.prod.yml build --no-cache
