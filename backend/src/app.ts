58d6f653b70dd40b8291b0681c3ebca4f5238d93
gnos28 <gnos28@gmail.com>
2023-04-21 15:40:59 +0200

    comment temporary bad imports

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import tasks from "./tasks";
+// import tasks from "./tasks";
 import * as dotenv from "dotenv";
 
 dotenv.config();
