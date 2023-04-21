d272758da6ba6358ec4121533c2bcba214e432f7
gnos28 <gnos28@gmail.com>
2023-04-21 15:40:36 +0200

    comment temporary bad imports

--- backend/src/tasks/index.ts
+++ backend/src/tasks/index.ts
@@ -1,21 +1,21 @@
-import CronJob from "node-cron";
-import { importPolyvalenceService } from "../services/getToken/getToken.core";
+// import CronJob from "node-cron";
+// import { importPolyvalenceService } from "../services/getToken/getToken.core";
 
-const initScheduledJobs = () => {
-  if (process.env.NODE_ENV !== "test") {
-    console.log("initScheduledJobs...");
+// const initScheduledJobs = () => {
+//   if (process.env.NODE_ENV !== "test") {
+//     console.log("initScheduledJobs...");
 
-    const every30min = "*/30 * * * *";
-    const every6min = "*/2 * * * *";
-    const at1pm = "0 13 * * *";
+//     const every30min = "*/30 * * * *";
+//     const every6min = "*/2 * * * *";
+//     const at1pm = "0 13 * * *";
 
-    const cronimportPolyvalenceFromPowerAutomateAndAdd = CronJob.schedule(
-      at1pm,
-      () => importPolyvalenceService()
-    );
+//     const cronimportPolyvalenceFromPowerAutomateAndAdd = CronJob.schedule(
+//       at1pm,
+//       () => importPolyvalenceService()
+//     );
 
-    cronimportPolyvalenceFromPowerAutomateAndAdd.start();
-  }
-};
+//     cronimportPolyvalenceFromPowerAutomateAndAdd.start();
+//   }
+// };
 
-export default { initScheduledJobs };
+// export default { initScheduledJobs };
