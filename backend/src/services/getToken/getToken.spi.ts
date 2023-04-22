6d14f41d05ab9d35615f68d401e6d8787cc6466c
gnos28 <gnos28@gmail.com>
2023-04-22 13:17:09 +0200

    infra & spi no longer sharing Types

--- backend/src/services/getToken/getToken.spi.ts
+++ backend/src/services/getToken/getToken.spi.ts
@@ -1,13 +1,27 @@
-import { getToken, GetTokenProps } from "../../infra/silae/getToken";
+import { getToken } from "../../infra/silae/getToken";
 
-type GetTokenPort = (props?: GetTokenProps) => ReturnType<typeof getToken>;
+type GetTokenProps = {
+  client_id: string;
+  client_secret: string;
+};
+
+type GetTokenResult = {
+  access_token: string;
+  token_type: string;
+  not_before: number;
+  expires_in: number;
+  expires_on: number;
+  resource: string;
+};
+
+type GetTokenPort = (props?: GetTokenProps) => Promise<GetTokenResult>;
 
 type GetTokenAdapter = {
   [key: string]: GetTokenPort;
 };
 
 const getTokenAdapter: GetTokenAdapter = {
-  silaeRest: async (props?: GetTokenProps) => await getToken(props),
+  silaeRest: async (props) => await getToken(props),
 };
 
 export { getTokenAdapter, GetTokenPort };
