7ea73b8bf2206f2cb7743f5947ad8f85304b4859
gnos28 <gnos28@gmail.com>
2023-04-22 11:59:32 +0200

    first test added

--- backend/src/services/getToken/getToken.test.ts
+++ backend/src/services/getToken/getToken.test.ts
@@ -1,7 +1,21 @@
-afterEach(() => {
-  jest.clearAllMocks();
+import { getTokenService } from "./getToken.core";
+import { GetTokenPort } from "./getToken.spi";
+
+const access_token = "token";
+
+const getTokenApiMock: GetTokenPort = async () => ({
+  access_token,
+  token_type: "token_type",
+  not_before: 0,
+  expires_in: 0,
+  expires_on: 0,
+  resource: "a",
 });
 
-describe("importEffectif", () => {
-  test("test", async () => {});
+describe("getTokenService", () => {
+  test("retrieve a token", async () => {
+    const token = await getTokenService(getTokenApiMock)();
+
+    expect(token).toBe("token");
+  });
 });
